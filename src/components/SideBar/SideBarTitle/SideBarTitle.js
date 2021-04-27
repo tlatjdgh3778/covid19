051080814ab620639e738d28587b921dddae7e01
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-04-27 17:12:17 +0900

    글자 크기 변경

--- src/components/SideBar/SideBarTitle/SideBarTitle.js
+++ src/components/SideBar/SideBarTitle/SideBarTitle.js
@@ -3,19 +3,28 @@ import styled from 'styled-components';
 
 const SideBarTitleContainer = styled.div`
 padding: 2rem 1rem;
-font-weight: 700;
+font-weight: 700; 
 font-size: ${(props) => props.theme.fontSize.md};
 
     @media ${(props) => props.theme.device.TabletPortrait}{
         padding: 0;
+        display: flex;
+    }
+`;
+const Title = styled.div`
+    @media ${(props) => props.theme.device.TabletPortrait}{
+        padding: 0 0.25rem;
     }
 `;
 
 function SideBarTitle() {
     return(
+        <>
         <SideBarTitleContainer>
-            COVID19 TRACKER
+            <Title>COVID 19 </Title>
+            <Title>TRACKER</Title>
         </SideBarTitleContainer>
+        </>
     );
 }
 export default SideBarTitle;
