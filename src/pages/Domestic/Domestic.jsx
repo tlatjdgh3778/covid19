583099a115ff4bc2bf68f16ab9487b3c4ec00cb6
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-24 20:10:39 +0900

    api 오류

--- src/pages/Domestic/Domestic.jsx
+++ src/pages/Domestic/Domestic.jsx
@@ -6,13 +6,14 @@ import * as GS from 'style/Component.style';
 const Domestic = ({koreaData, cityData, isDark}) => {
     return(
         <>
-            <GS.LeftContainer>
+            {/* <GS.LeftContainer>
                 <DomesticStatus koreaData={koreaData} cityData={cityData}></DomesticStatus>
                 <DomesticCityStatus cityData={cityData}></DomesticCityStatus>
             </GS.LeftContainer>
             <GS.RightContainer>
                 <Map koreaData={koreaData} cityData={cityData} isDark={isDark}></Map>
-            </GS.RightContainer>  
+            </GS.RightContainer>   */}
+            <div>국내 api 주소 작업 중</div>
         </>
     );
 }
