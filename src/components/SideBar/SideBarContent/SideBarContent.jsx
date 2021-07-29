9e44261caf90938d407e6f1c9f42dc4229144345
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-29 21:13:02 +0900

    refactor : arrow function 으로 변경

--- src/components/SideBar/SideBarContent/SideBarContent.jsx
+++ src/components/SideBar/SideBarContent/SideBarContent.jsx
@@ -1,27 +1,24 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import * as S from './SideBarContent.style';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import * as S from "./SideBarContent.style";
 
-function SideBarContent() {
-
-    return(
+const SideBarContent = () => {
+    return (
         <S.SideBarContentContainer>
             <NavLink exact to="/">
                 <S.DomesticContainer>
-                    <S.HomeIcon/>
-                    <S.DomesticStatus>
-                    국내 현황
-                    </S.DomesticStatus>
-                </S.DomesticContainer>   
+                    <S.HomeIcon />
+                    <S.DomesticStatus>국내 현황</S.DomesticStatus>
+                </S.DomesticContainer>
             </NavLink>
             <NavLink to="/world">
-                <S.WorldContainer> 
-                    <S.WorldIcon/>
+                <S.WorldContainer>
+                    <S.WorldIcon />
                     <S.WorldStatus>세계 현황</S.WorldStatus>
-                </S.WorldContainer>    
+                </S.WorldContainer>
             </NavLink>
         </S.SideBarContentContainer>
     );
-}
+};
 
 export default SideBarContent;
