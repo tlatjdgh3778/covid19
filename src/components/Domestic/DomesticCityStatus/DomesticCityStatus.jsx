16ced84d9d1a968d1fd96a1aa38da50036a07f6a
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-30 21:58:52 +0900

    feat : react-redux로 상태 관리하도록 변경

--- src/components/Domestic/DomesticCityStatus/DomesticCityStatus.jsx
+++ src/components/Domestic/DomesticCityStatus/DomesticCityStatus.jsx
@@ -1,45 +1,64 @@
-import React from 'react';
-import * as S from './DomesticCityStatus.style';
+import React from "react";
+import * as S from "./DomesticCityStatus.style";
+import { useSelector } from "react-redux";
 
-const DomesticCityStatus = ({cityData}) => {
+const DomesticCityStatus = () => {
+    const cityData = useSelector(({ korea }) => korea.cityData.data);
 
-const ew = Object.entries(cityData);
-    return(
+    const newData = Object.entries(cityData).slice(2, 20);
+
+    const total = newData.splice(0, 1); // 합계
+    newData.splice(newData.length, 0, total[0]); // 표의 제일 밑으로 보내기
+
+    // arr.splice(to, 0, item[0]); item 을 to 위치로 이동.
+    // const arr = [...newData];
+    // const item = arr.splice(0, 1); //
+    // arr.splice(newData.length, 0, item[0]);
+    // console.log(arr);
+    return (
         <>
-        <S.CityContainer>시 도 별 현황</S.CityContainer>
-        <S.TableContainer>
-            <S.TitleContainer>
-                <S.TableTitleCity>지역</S.TableTitleCity>
-                <S.TableTitle>확진자</S.TableTitle>
-                <S.TableTitle>완치자</S.TableTitle>
-                <S.TableTitle>사망자</S.TableTitle>
-                <S.TableTitle>발생률</S.TableTitle>
-            </S.TitleContainer>
-            {ew.map((data, i)=>{
-                if(i%2 === 0){
-                    return(
-                        <S.TitleContainer key={i}>
-                            <S.CityName>{data[1].countryName}</S.CityName>
-                            <S.TotalCase>{data[1].totalCase}</S.TotalCase>
-                            <S.Recovered>{data[1].recovered}</S.Recovered>
-                            <S.Death>{data[1].death}</S.Death>
-                            <S.Percentage>{data[1].percentage}</S.Percentage>
-                        </S.TitleContainer>
-                    )
-                }else{
-                    return(
-                        <S.TitleContainer key={i}>
-                            <S.CityName>{data[1].countryName}</S.CityName>
-                            <S.TotalCase bgColor>{data[1].totalCase}</S.TotalCase>
-                            <S.Recovered bgColor>{data[1].recovered}</S.Recovered>
-                            <S.Death bgColor>{data[1].death}</S.Death>
-                            <S.Percentage bgColor>{data[1].percentage}</S.Percentage>
-                        </S.TitleContainer>
-                    )
-                }
-            })}
-        </S.TableContainer>
+            <S.CityContainer>시 도 별 현황</S.CityContainer>
+            <S.TableContainer>
+                <S.TitleContainer>
+                    <S.TableTitleCity>지역</S.TableTitleCity>
+                    <S.TableTitle>확진자</S.TableTitle>
+                    <S.TableTitle>완치자</S.TableTitle>
+                    <S.TableTitle>사망자</S.TableTitle>
+                    <S.TableTitle>발생률</S.TableTitle>
+                </S.TitleContainer>
+                {newData.map((data, i) => {
+                    if (i % 2 === 0) {
+                        return (
+                            <S.TitleContainer key={i}>
+                                <S.CityName>{data[1].countryName}</S.CityName>
+                                <S.TotalCase>{data[1].totalCase}</S.TotalCase>
+                                <S.Recovered>{data[1].recovered}</S.Recovered>
+                                <S.Death>{data[1].death}</S.Death>
+                                <S.Percentage>
+                                    {data[1].percentage}
+                                </S.Percentage>
+                            </S.TitleContainer>
+                        );
+                    } else {
+                        return (
+                            <S.TitleContainer key={i}>
+                                <S.CityName>{data[1].countryName}</S.CityName>
+                                <S.TotalCase bgColor>
+                                    {data[1].totalCase}
+                                </S.TotalCase>
+                                <S.Recovered bgColor>
+                                    {data[1].recovered}
+                                </S.Recovered>
+                                <S.Death bgColor>{data[1].death}</S.Death>
+                                <S.Percentage bgColor>
+                                    {data[1].percentage}
+                                </S.Percentage>
+                            </S.TitleContainer>
+                        );
+                    }
+                })}
+            </S.TableContainer>
         </>
-    )
-}
+    );
+};
 export default DomesticCityStatus;
