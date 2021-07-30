5973d4eb0e704ef1ee0b7fa360c8a79057935b18
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-30 23:38:02 +0900

    feat : loading spinner 적용

--- src/pages/Domestic/Domestic.jsx
+++ src/pages/Domestic/Domestic.jsx
@@ -2,17 +2,28 @@ import React from "react";
 import { DomesticStatus, DomesticCityStatus } from "components/Domestic/index";
 import Map from "components/Map/Map";
 import * as GS from "style/Component.style";
+import Loading from "components/Loading/Loading";
+import { useSelector } from "react-redux";
 
 const Domestic = () => {
+    const loadingKorea = useSelector(({ korea }) => korea.koreaData.loading);
+    const loadingCity = useSelector(({ korea }) => korea.cityData.loading);
+
     return (
         <>
-            <GS.LeftContainer>
-                <DomesticStatus></DomesticStatus>
-                <DomesticCityStatus></DomesticCityStatus>
-            </GS.LeftContainer>
-            <GS.RightContainer>
-                <Map></Map>
-            </GS.RightContainer>
+            {loadingKorea || loadingCity ? (
+                <Loading />
+            ) : (
+                <>
+                    <GS.LeftContainer>
+                        <DomesticStatus></DomesticStatus>
+                        <DomesticCityStatus></DomesticCityStatus>
+                    </GS.LeftContainer>
+                    <GS.RightContainer>
+                        <Map></Map>
+                    </GS.RightContainer>
+                </>
+            )}
         </>
     );
 };
