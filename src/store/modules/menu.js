a2489d74900cc785ca215a41934efd38bd2e846a
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-30 19:44:14 +0900

    feat : ducks pattern 적용

---
+++ src/store/modules/menu.js
@@ -0,0 +1,28 @@
+// menu module
+
+// actionTypes
+export const TOGGLE_MENU = "menu/TOGGLE_MENU";
+
+// action creator
+export const toggleMenu = () => {
+    return {
+        type: TOGGLE_MENU,
+    };
+};
+
+// reducer
+const initialState = {
+    isOpen: false,
+};
+
+export default function reducer(state = initialState, action) {
+    switch (action.type) {
+        case TOGGLE_MENU:
+            return {
+                ...state,
+                isOpen: !state.isOpen,
+            };
+        default:
+            return state;
+    }
+}
---
+++ src/store/modules/mode.js
@@ -0,0 +1,28 @@
+// mode module
+
+// actionTypes
+export const CHANGE_MODE = "mode/CHANGE_MODE";
+
+// action creator
+export const changeMode = () => {
+    return {
+        type: CHANGE_MODE,
+    };
+};
+
+// reducer
+const initialState = {
+    isDark: false,
+};
+
+export default function reducer(state = initialState, action) {
+    switch (action.type) {
+        case CHANGE_MODE:
+            return {
+                ...state,
+                isDark: !state.isDark,
+            };
+        default:
+            return state;
+    }
+}
