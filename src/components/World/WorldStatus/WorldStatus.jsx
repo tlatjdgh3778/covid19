2bd222d6c626f52b56a0c49e39b0ec3765d08ec1
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-31 22:38:49 +0900

    feat : react-redux로 상태 관리하도록 변경 및 코드 리팩토링

--- src/components/World/WorldStatus/WorldStatus.jsx
+++ src/components/World/WorldStatus/WorldStatus.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import * as GS from "style/Component.style";
-import { useSelector } from "react-redux";
-
+import { useSelector, useDispatch } from "react-redux";
+import { changeState } from "store/modules/state";
 const WorldStatus = () => {
+    const dispatch = useDispatch();
     const worldData = useSelector(({ world }) => world.worldData.data);
+    const currentState = useSelector(({ state }) => state.currentState);
 
     const names = ["확진환자", "격리해제", "사망자"];
+    const data = ["cases", "recovered", "deaths"];
+    const todayData = ["todayCases", "todayRecovered", "todayDeaths"];
+
+    const onClick = (e) => {
+        dispatch(changeState(e.currentTarget.childNodes[0].innerHTML));
+    };
 
     return (
         <>
@@ -13,61 +21,28 @@ const WorldStatus = () => {
             <GS.StatusContainer>
                 {names.map((name, i) => {
                     return (
-                        <GS.TotalContainer key={i}>
-                            {
-                                <>
-                                    <GS.Title>{name}</GS.Title>
-                                    {name === "확진환자" ? (
-                                        <>
-                                            <GS.Data color={"#df0736"}>
-                                                {worldData.cases.toLocaleString()}
-                                            </GS.Data>
-                                            <GS.TodayData color={"#df0736"}>
-                                                <div>
-                                                    {worldData.cases > 0
-                                                        ? "+"
-                                                        : ""}
-                                                </div>
-                                                <div>
-                                                    {worldData.todayCases.toLocaleString()}
-                                                </div>
-                                            </GS.TodayData>
-                                        </>
-                                    ) : name === "격리해제" ? (
-                                        <>
-                                            <GS.Data color={"#27a643"}>
-                                                {worldData.recovered.toLocaleString()}
-                                            </GS.Data>
-                                            <GS.TodayData color={"#27a643"}>
-                                                <div>
-                                                    {worldData.recovered > 0
-                                                        ? "+"
-                                                        : ""}
-                                                </div>
-                                                <div>
-                                                    {worldData.todayRecovered.toLocaleString()}
-                                                </div>
-                                            </GS.TodayData>
-                                        </>
-                                    ) : (
-                                        <>
-                                            <GS.Data>
-                                                {worldData.deaths.toLocaleString()}
-                                            </GS.Data>
-                                            <GS.TodayData>
-                                                <div>
-                                                    {worldData.deaths > 0
-                                                        ? "+"
-                                                        : ""}
-                                                </div>
-                                                <div>
-                                                    {worldData.todayDeaths.toLocaleString()}
-                                                </div>
-                                            </GS.TodayData>
-                                        </>
-                                    )}
-                                </>
-                            }
+                        <GS.TotalContainer
+                            onClick={onClick}
+                            key={i}
+                            color={name}
+                            activeColor={currentState}
+                        >
+                            <GS.Title>{name}</GS.Title>
+                            <>
+                                <GS.Data color={name}>
+                                    {worldData[data[i]].toLocaleString()}
+                                </GS.Data>
+                                <GS.TodayData color={name}>
+                                    <div>
+                                        {worldData[data[i]] > 0 ? "+" : ""}
+                                    </div>
+                                    <div>
+                                        {worldData[
+                                            todayData[i]
+                                        ].toLocaleString()}
+                                    </div>
+                                </GS.TodayData>
+                            </>
                         </GS.TotalContainer>
                     );
                 })}
