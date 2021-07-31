3f1530d8366b77c0737dbe1290814caf3312833f
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-31 15:34:43 +0900

    style : 컴포넌트 style 변경

--- src/components/World/WorldStatusChart/WorldStatusChart.style.js
+++ src/components/World/WorldStatusChart/WorldStatusChart.style.js
@@ -1,29 +1,25 @@
-import styled, { css } from 'styled-components';
-import numeral from 'numeral';
+import styled, { css } from "styled-components";
+import numeral from "numeral";
 
 const ChartStyle = css`
-border-radius: 10px;
-margin: 1rem 0;
-padding: 0.5rem 0;
+    border-radius: 10px;
+    margin: 1rem 0;
+    padding: 0.5rem 0;
 `;
 const ChartTitle = css`
-margin: 0.5rem;
-font-weight: 700;
-text-align: center;
+    margin: 0.5rem;
+    font-weight: 700;
+    text-align: center;
 `;
 
 const options = {
     plugins: {
         title: {
             display: true,
-            text: 'Custom Chart Title'
-        }
+            text: "Custom Chart Title",
+        },
     },
     legend: {
-        // labels: {
-        //     fontSize: 16,
-        //     fontWeight: 900,
-        // }
         display: false,
     },
     elements: {
@@ -31,7 +27,6 @@ const options = {
             radius: 0,
         },
     },
-    // maintainAspectRatio: false,
 
     tooltips: {
         mode: "index",
@@ -43,13 +38,13 @@ const options = {
                 gridLines: {
                     display: false,
                 },
-            }
+            },
         ],
         yAxes: [
             {
                 gridLines: {
                     display: false,
-                },       
+                },
                 ticks: {
                     callback: function (value, index, values) {
                         return numeral(value).format("0a");
@@ -58,37 +53,48 @@ const options = {
             },
         ],
     },
-}
+};
 const CasesChartContainer = styled.div`
-${ChartStyle};
-background-color: ${(props) => props.theme.color.chartRed};
+    ${ChartStyle};
+    background-color: ${(props) => props.theme.color.chartRed};
 `;
 const RecoveredChartContainer = styled.div`
-${ChartStyle};
-background-color: ${(props) => props.theme.color.chartGreen};
+    ${ChartStyle};
+    background-color: ${(props) => props.theme.color.chartGreen};
 `;
 const DeathsChartContainer = styled.div`
-${ChartStyle};
-background-color: ${(props) => props.theme.color.chartColor};
+    ${ChartStyle};
+    background-color: ${(props) => props.theme.color.chartColor};
 `;
 const CasesTitle = styled.div`
-${ChartTitle}
-color: ${(props) => props.theme.color.fontRed};
+    ${ChartTitle}
+    color: ${(props) => props.theme.color.fontRed};
 `;
 const RecoveredTitle = styled.div`
-${ChartTitle}
-color: ${(props) => props.theme.color.fontGreen};
+    ${ChartTitle}
+    color: ${(props) => props.theme.color.fontGreen};
 `;
 const DeathsTitle = styled.div`
-${ChartTitle}
-color: ${(props) => props.theme.color.fontColor};
+    ${ChartTitle}
+    color: ${(props) => props.theme.color.fontColor};
 `;
 
 const Title = styled.div`
-margin: 1rem;
-padding-bottom: 1rem;
-font-weight: 700;
-font-size: ${(props) => props.theme.fontSize.lg};
+    margin: 1rem;
+    padding-bottom: 1rem;
+    font-weight: 700;
+    font-size: ${(props) => props.theme.fontSize.lg};
 `;
 
-export { ChartStyle, ChartTitle, options, CasesChartContainer, RecoveredChartContainer, DeathsChartContainer, CasesTitle, RecoveredTitle, DeathsTitle, Title };
+export {
+    ChartStyle,
+    ChartTitle,
+    options,
+    CasesChartContainer,
+    RecoveredChartContainer,
+    DeathsChartContainer,
+    CasesTitle,
+    RecoveredTitle,
+    DeathsTitle,
+    Title,
+};
--- src/style/Component.style.js
+++ src/style/Component.style.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from "styled-components";
 
 const MainContainer = styled.div`
     display: flex;
@@ -9,11 +9,11 @@ const MainContainer = styled.div`
     margin-right: 3rem;
     padding-top: 5rem;
     color: ${(props) => props.theme.color.fontColor};
-  
-    @media ${(props) => props.theme.device.TabletLandscape}{
+
+    @media ${(props) => props.theme.device.TabletLandscape} {
         margin-left: 9rem;
     }
-    @media ${(props) => props.theme.device.TabletPortrait}{
+    @media ${(props) => props.theme.device.TabletPortrait} {
         display: flex;
         flex-direction: column;
         margin-left: 0;
@@ -25,11 +25,14 @@ const MainContainer = styled.div`
 `;
 
 const RightContainer = styled.div`
+    width: 37rem;
+    margin: 0 3rem;
     color: ${(props) => props.theme.color.fontColor};
-    margin: 1rem;
-    min-height: 60rem;
 
-    @media ${(props) => props.theme.device.TabletPortrait}{
+    @media ${(props) => props.theme.device.TabletLandscape} {
+        margin: 1rem;
+    }
+    @media ${(props) => props.theme.device.TabletPortrait} {
         display: -webkit-flex;
         display: flex;
         flex-direction: column;
@@ -39,13 +42,14 @@ const RightContainer = styled.div`
 `;
 
 const LeftContainer = styled.div`
+    width: 37rem;
     margin: 0 3rem;
     color: ${(props) => props.theme.color.fontColor};
 
-    @media ${(props) => props.theme.device.TabletLandscape}{
+    @media ${(props) => props.theme.device.TabletLandscape} {
         margin: 1rem;
     }
-    @media ${(props) => props.theme.device.TabletPortrait}{
+    @media ${(props) => props.theme.device.TabletPortrait} {
         display: flex;
         flex-direction: column;
         margin: 0;
@@ -54,4 +58,81 @@ const LeftContainer = styled.div`
     }
 `;
 
-export { MainContainer, RightContainer, LeftContainer };
+const StatusContainer = styled.div`
+    display: flex;
+    margin: 0 1rem;
+    justify-content: space-around;
+`;
+
+const StatusTitle = styled.div`
+    margin: 1rem;
+    padding-bottom: 1rem;
+    font-weight: 700;
+    font-size: ${(props) => props.theme.fontSize.lg};
+    border-bottom: 1px solid ${(props) => props.theme.color.fontColor};
+`;
+const container = css`
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0.5rem 1rem;
+    height: 5rem;
+    justify-content: space-between;
+    cursor: default;
+
+    @media ${(props) => props.theme.device.TabletPortrait} {
+        margin: 0;
+    }
+    @media ${(props) => props.theme.device.MobileLandscape} {
+        width: 3rem;
+        height: 4rem;
+        padding: 0.25rem;
+    }
+`;
+
+const TotalContainer = styled.div`
+    ${container};
+    &:hover {
+        background-color: ${(props) => props.theme.color.hoverColor};
+    }
+`;
+
+const Title = styled.div`
+    @media ${(props) => props.theme.device.TabletPortrait} {
+        font-size: ${(props) => props.theme.fontSize.ssm};
+    }
+`;
+
+const Data = styled.div`
+    font-size: ${(props) => props.theme.fontSize.lg};
+    font-weight: 700;
+    color: ${(props) => props.color || props.theme.color.fontColor};
+
+    @media ${(props) => props.theme.device.TabletPortrait} {
+        font-size: ${(props) => props.theme.fontSize.md};
+    }
+    @media ${(props) => props.theme.device.MobileLandscape} {
+        font-size: ${(props) => props.theme.fontSize.sm};
+    }
+`;
+const TodayData = styled.div`
+    color: ${(props) => props.color || props.theme.color.fontColor};
+    display: flex;
+
+    @media ${(props) => props.theme.device.TabletPortrait} {
+        font-size: ${(props) => props.theme.fontSize.ssm};
+    }
+`;
+
+export {
+    MainContainer,
+    RightContainer,
+    LeftContainer,
+    StatusContainer,
+    StatusTitle,
+    TotalContainer,
+    Title,
+    Data,
+    TodayData,
+};
