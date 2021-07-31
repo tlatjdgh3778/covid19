dd6150f89c94a913fde1e5f0d81f678febfe67a5
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-31 13:47:40 +0900

    refactor : toLocaleString() 적용. (천 단위마다 점)

--- src/components/World/WorldStatus/WorldStatus.jsx
+++ src/components/World/WorldStatus/WorldStatus.jsx
@@ -1,44 +1,79 @@
-import React from 'react';
-import * as S from './WorldStatus.style';
+import React from "react";
+import * as S from "./WorldStatus.style";
+import { useSelector } from "react-redux";
 
-const WorldStatus = ({ worldData }) => {
-    const names = ['확진환자', '격리해제', '사망자'];
+const WorldStatus = () => {
+    const worldData = useSelector(({ world }) => world.worldData.data);
 
-    return(
+    const names = ["확진환자", "격리해제", "사망자"];
+
+    return (
         <S.WorldDataContainer>
             <S.DomesticStatusTitle>세계현황</S.DomesticStatusTitle>
             <S.DomesticStatusContainer>
-                {names.map((name, i)=>{
-                    return(
-                        <S.TotalContainer key={i} >
+                {names.map((name, i) => {
+                    return (
+                        <S.TotalContainer key={i}>
                             {
-                            <>
-                            <S.Title>{name}</S.Title>
-                                {name==='확진환자'?
                                 <>
-                                <S.Data color={'#df0736'}>
-                                    {worldData.confirmed.toLocaleString()}
-                                </S.Data>                      
+                                    <S.Title>{name}</S.Title>
+                                    {name === "확진환자" ? (
+                                        <>
+                                            <S.Data color={"#df0736"}>
+                                                {worldData.cases.toLocaleString()}
+                                            </S.Data>
+                                            <S.TodayData color={"#df0736"}>
+                                                <div>
+                                                    {worldData.cases > 0
+                                                        ? "+"
+                                                        : ""}
+                                                </div>
+                                                <div>
+                                                    {worldData.todayCases.toLocaleString()}
+                                                </div>
+                                            </S.TodayData>
+                                        </>
+                                    ) : name === "격리해제" ? (
+                                        <>
+                                            <S.Data color={"#27a643"}>
+                                                {worldData.recovered.toLocaleString()}
+                                            </S.Data>
+                                            <S.TodayData color={"#27a643"}>
+                                                <div>
+                                                    {worldData.recovered > 0
+                                                        ? "+"
+                                                        : ""}
+                                                </div>
+                                                <div>
+                                                    {worldData.todayRecovered.toLocaleString()}
+                                                </div>
+                                            </S.TodayData>
+                                        </>
+                                    ) : (
+                                        <>
+                                            <S.Data>
+                                                {worldData.deaths.toLocaleString()}
+                                            </S.Data>
+                                            <S.TodayData>
+                                                <div>
+                                                    {worldData.deaths > 0
+                                                        ? "+"
+                                                        : ""}
+                                                </div>
+                                                <div>
+                                                    {worldData.todayDeaths.toLocaleString()}
+                                                </div>
+                                            </S.TodayData>
+                                        </>
+                                    )}
                                 </>
-                                :
-                                (
-                                name==='격리해제'?
-                                <>
-                                <S.Data color={'#27a643'}>{worldData.recovered.toLocaleString()}</S.Data>
-                                </>
-                                :
-                                <>
-                                <S.Data>{worldData.deaths.toLocaleString()}</S.Data>
-                                </>
-                                )}
-                            </>
                             }
                         </S.TotalContainer>
-                    )
+                    );
                 })}
             </S.DomesticStatusContainer>
         </S.WorldDataContainer>
     );
-}
+};
 
 export default WorldStatus;
