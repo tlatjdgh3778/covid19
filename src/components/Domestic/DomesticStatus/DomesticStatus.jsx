3fb7f17ce45d2c9934074b830ea0738b803e62a2
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-07-31 22:36:05 +0900

    feat : react-redux로 상태 관리하도록 변경 및 코드 리팩토링

--- src/components/Domestic/DomesticStatus/DomesticStatus.jsx
+++ src/components/Domestic/DomesticStatus/DomesticStatus.jsx
@@ -1,94 +1,78 @@
 import React from "react";
 import * as GS from "style/Component.style";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { changeState } from "store/modules/state";
 
 const DomesticStatus = () => {
     // 누적 확진자 : TotalCase, city_url.korea.newCase
     // 격리해제(완치) : TotalRecovered, TodayRecovered
     // 격리중(치료중) : NowCase, TotalCaseBefore
     // 사망자 수 : TotalDeath, TodayDeath
-
-    const names = ["확진환자", "치료중", "격리해제", "사망자"];
+    const dispatch = useDispatch();
+    const names = ["치료중", "격리해제", "사망자"];
     const koreaData = useSelector(({ korea }) => korea.koreaData.data);
     const cityData = useSelector(({ korea }) => korea.cityData.data);
+    const currentState = useSelector(({ state }) => state.currentState);
+
+    const onClick = (e) => {
+        dispatch(changeState(e.currentTarget.childNodes[0].innerHTML));
+    };
+
+    const data = ["NowCase", "TotalRecovered", "TotalDeath"];
+    const todayData = ["TotalCaseBefore", "TodayRecovered", "TodayDeath"];
 
     return (
         <>
             <GS.StatusTitle>국내현황</GS.StatusTitle>
             <GS.StatusContainer>
+                <GS.TotalContainer
+                    onClick={onClick}
+                    color="확진환자"
+                    activeColor={currentState}
+                >
+                    {
+                        <>
+                            <GS.Title>확진환자</GS.Title>
+                            <GS.Data color="확진환자">
+                                {koreaData.TotalCase}
+                            </GS.Data>
+                            <GS.TodayData color="확진환자">
+                                <div>
+                                    {cityData.korea.newCase.replace(",", "") > 0
+                                        ? "+"
+                                        : ""}
+                                </div>
+                                <div>{cityData.korea.newCase}</div>
+                            </GS.TodayData>
+                        </>
+                    }
+                </GS.TotalContainer>
                 {names.map((name, i) => {
                     return (
-                        <GS.TotalContainer key={i}>
+                        <GS.TotalContainer
+                            key={i}
+                            onClick={onClick}
+                            color={name}
+                            activeColor={currentState}
+                        >
                             {
                                 <>
                                     <GS.Title>{name}</GS.Title>
-                                    {name === "확진환자" ? (
-                                        <>
-                                            <GS.Data color={"#df0736"}>
-                                                {koreaData.TotalCase.toLocaleString()}
-                                            </GS.Data>
-                                            <GS.TodayData color={"#df0736"}>
-                                                <div>
-                                                    {cityData.korea.newCase > 0
-                                                        ? "+"
-                                                        : ""}
-                                                </div>
-                                                <div>
-                                                    {cityData.korea.newCase.toLocaleString()}
-                                                </div>
-                                            </GS.TodayData>
-                                        </>
-                                    ) : name === "치료중" ? (
-                                        <>
-                                            <GS.Data color={"#0562cb"}>
-                                                {koreaData.NowCase.toLocaleString()}
-                                            </GS.Data>
-                                            <GS.TodayData color={"#0562cb"}>
-                                                <div>
-                                                    {koreaData.TotalCaseBefore >
-                                                    0
-                                                        ? "+"
-                                                        : ""}
-                                                </div>
-                                                <div>
-                                                    {koreaData.TotalCaseBefore.toLocaleString()}
-                                                </div>
-                                            </GS.TodayData>
-                                        </>
-                                    ) : name === "격리해제" ? (
-                                        <>
-                                            <GS.Data color={"#27a643"}>
-                                                {koreaData.TotalRecovered.toLocaleString()}
-                                            </GS.Data>
-                                            <GS.TodayData color={"#27a643"}>
-                                                <div>
-                                                    {koreaData.TodayRecovered >
-                                                    0
-                                                        ? "+"
-                                                        : ""}
-                                                </div>
-                                                <div>
-                                                    {koreaData.TodayRecovered.toLocaleString()}
-                                                </div>
-                                            </GS.TodayData>
-                                        </>
-                                    ) : (
-                                        <>
-                                            <GS.Data>
-                                                {koreaData.TotalDeath.toLocaleString()}
-                                            </GS.Data>
-                                            <GS.TodayData>
-                                                <div>
-                                                    {koreaData.TodayDeath > 0
-                                                        ? "+"
-                                                        : ""}
-                                                </div>
-                                                <div>
-                                                    {koreaData.TodayDeath.toLocaleString()}
-                                                </div>
-                                            </GS.TodayData>
-                                        </>
-                                    )}
+                                    <GS.Data color={name}>
+                                        {koreaData[data[i]]}
+                                    </GS.Data>
+                                    <GS.TodayData color={name}>
+                                        <div>
+                                            {koreaData[todayData[i]] > 0
+                                                ? "+"
+                                                : ""}
+                                        </div>
+                                        <div>
+                                            {Number(
+                                                koreaData[todayData[i]]
+                                            ).toLocaleString()}
+                                        </div>
+                                    </GS.TodayData>
                                 </>
                             }
                         </GS.TotalContainer>
