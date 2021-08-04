cfb047184d34db0a0759e1a0e2391636bbb184a7
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-08-04 21:12:13 +0900

    style : 메뉴 아이템 컨테이너 중복 최소화

--- src/components/SideBar/SideBarContent/SideBarContent.style.js
+++ src/components/SideBar/SideBarContent/SideBarContent.style.js
@@ -1,50 +1,34 @@
-import styled from 'styled-components';
-import { Home, World } from '@styled-icons/boxicons-regular';
+import styled from "styled-components";
+import { Home, World } from "@styled-icons/boxicons-regular";
 
 const SideBarContentContainer = styled.div`
-text-align: center;
-width: 100%;
+    text-align: center;
+    width: 100%;
 
-    @media ${(props) => props.theme.device.TabletPortrait}{
+    @media ${(props) => props.theme.device.TabletPortrait} {
         display: none;
     }
 `;
 
-const DomesticContainer = styled.div`
-display: flex;
-cursor: pointer;
-align-items: center;
-margin: 1rem 0;
-padding: 0.5rem 1rem;
+const MenuContainer = styled.div`
+    display: flex;
+    cursor: pointer;
+    align-items: center;
+    justify-content: center;
+    margin: 1rem 0;
+    padding: 0.5rem 1rem;
     &:hover {
         background-color: ${(props) => props.theme.color.hoverColor};
     }
-
-`;
-const DomesticStatus = styled.div`
 `;
 const HomeIcon = styled(Home)`
-margin-right: 0.5rem;
-width: 2rem;
-padding-left: 0.5rem;
+    margin-right: 0.5rem;
+    width: 2rem;
 `;
 
-const WorldContainer = styled.div`
-display: flex;
-cursor: pointer;
-align-items: center;
-margin: 1rem 0;
-padding: 0.5rem 1rem;
-    &:hover {
-        background-color: ${(props) => props.theme.color.hoverColor};
-    }
-`;
-const WorldStatus = styled.div`
-`;
 const WorldIcon = styled(World)`
-margin-right: 0.5rem;
-width: 2rem;
-padding-left: 0.5rem;
+    margin-right: 0.5rem;
+    width: 2rem;
 `;
 
-export { SideBarContentContainer, DomesticContainer, DomesticStatus, HomeIcon, WorldContainer, WorldStatus,WorldIcon };
+export { SideBarContentContainer, MenuContainer, HomeIcon, WorldIcon };
