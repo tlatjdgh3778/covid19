1a6e9945395b7eb469cc8cc2fa0c95d4d945c361
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-08-04 20:37:46 +0900

    feat : react-redux로 상태 관리하도록 변경

--- src/components/World/WorldStatusChart/WorldStatusChart.jsx
+++ src/components/World/WorldStatusChart/WorldStatusChart.jsx
@@ -1,86 +1,77 @@
-import React, { useState, useEffect } from 'react';
-import { Line } from 'react-chartjs-2';
-import * as S from './WorldStatusChart.style';
+import React from "react";
+import { Line } from "react-chartjs-2";
+import * as S from "./WorldStatusChart.style";
+import { useSelector } from "react-redux";
 
 const WorldStatusChart = () => {
-    const [dailyData, setDailyData] = useState({});
+    const dailyData = useSelector(
+        ({ worldDaily }) => worldDaily.dailyData.data
+    );
 
-    const url = 'https://disease.sh/v3/covid-19/historical/all?lastdays=90';
-
-    useEffect(()=>{
-        const getData = async () => {
-
-            const res = await fetch(url);
-            const res_data = await res.json();
-    
-            setDailyData(res_data);
-        }
-        getData();
-    }, []);
+    const daily = Object.entries(dailyData);
 
     let dateLabels;
     let casesData;
     let deathsData;
     let recoveredData;
-    const ew = Object.entries(dailyData);
-    console.log(Object.keys(ew));
-    console.log(ew.length);
 
-    if(ew.length !== 0){
-        dateLabels = Object.keys(ew[0][1]);
-        casesData = Object.values(ew[0][1]);
-        deathsData = Object.values(ew[1][1]);
-        recoveredData = Object.values(ew[2][1]);
+    if (daily.length !== 0) {
+        dateLabels = Object.keys(daily[0][1]);
+        casesData = Object.values(daily[0][1]);
+        deathsData = Object.values(daily[1][1]);
+        recoveredData = Object.values(daily[2][1]);
     }
     const casesChart = {
         labels: dateLabels,
         datasets: [
-          {
-            label: "확진자 수",
-            data: casesData,
-            fill: false,
-            borderColor: "#df0736",
-          },]
-      };
-      const recoveredChart = {
+            {
+                label: "확진자 수",
+                data: casesData,
+                fill: false,
+                borderColor: "#df0736",
+            },
+        ],
+    };
+    const recoveredChart = {
         labels: dateLabels,
         datasets: [
-          {
-            label: "완치자 수",
-            data: recoveredData,
-            fill: false,
-            borderColor: "#27a643",
-
-          },]
-      };
-      const deathsChart = {
+            {
+                label: "격리해제자 수",
+                data: recoveredData,
+                fill: false,
+                borderColor: "#27a643",
+            },
+        ],
+    };
+    const deathsChart = {
         labels: dateLabels,
         datasets: [
-          {
-            label: "사망자 수",
-            data: deathsData,
-            fill: false,
-            borderColor: "#373737",
-          },],
-      };
+            {
+                label: "사망자 수",
+                data: deathsData,
+                fill: false,
+                borderColor: "#373737",
+            },
+        ],
+    };
 
-    return(
+    return (
         <>
-        <S.Title>전 세계 코로나 차트</S.Title>
-        <S.CasesChartContainer>
-            <S.CasesTitle>확진자 수</S.CasesTitle>
-            <Line data={casesChart} options={S.options}></Line>
-        </S.CasesChartContainer>
-        <S.RecoveredChartContainer>
-            <S.RecoveredTitle>완치자 수</S.RecoveredTitle>
-            <Line data={recoveredChart} options={S.options}></Line>
-        </S.RecoveredChartContainer>
-        <S.DeathsChartContainer>
-            <S.DeathsTitle>사망자 수</S.DeathsTitle>
-            <Line data={deathsChart} options={S.options}></Line>
-        </S.DeathsChartContainer>
+            <S.Title>전 세계 코로나 차트</S.Title>
+            <S.CasesChartContainer>
+                <S.CasesTitle>확진자 수</S.CasesTitle>
+                <Line data={casesChart} options={S.options}></Line>
+            </S.CasesChartContainer>
+            <S.RecoveredChartContainer>
+                <S.RecoveredTitle>완치자 수</S.RecoveredTitle>
+                <Line data={recoveredChart} options={S.options}></Line>
+            </S.RecoveredChartContainer>
+            <S.DeathsChartContainer>
+                <S.DeathsTitle>사망자 수</S.DeathsTitle>
+                <Line data={deathsChart} options={S.options}></Line>
+            </S.DeathsChartContainer>
         </>
     );
-}
+};
 
 export default WorldStatusChart;
