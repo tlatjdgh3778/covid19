ec9df2131aa3faa1c059932978c7b9fa65e63215
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2021-08-18 21:48:33 +0900

    api key test(401)

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,7 +1,7 @@
-const API_KEY = process.env.REACT_APP_API_KEY;
+// const API_KEY = process.env.REACT_APP_API_KEY;
 
-export const koreaUrl = `https://api.corona-19.kr/korea/?serviceKey=${API_KEY}`;
-export const cityUrl = `https://api.corona-19.kr/korea/country/new/?serviceKey=${API_KEY}`;
+export const koreaUrl = `https://api.corona-19.kr/korea/?serviceKey=RNZOsrb1kf6D89weJtm74ABhzSjcMI2Ku`;
+export const cityUrl = `https://api.corona-19.kr/korea/country/new/?serviceKey=RNZOsrb1kf6D89weJtm74ABhzSjcMI2Ku`;
 export const worldUrl = "https://disease.sh/v3/covid-19/all";
 export const countriesUrl = "https://disease.sh/v3/covid-19/countries";
 export const worldDaily = "https://disease.sh/v3/covid-19/historical/all?lastdays=90";
